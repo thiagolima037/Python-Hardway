e505922d9f8041c8b31ce7f9c94130bc1c4a0415
Thiago Lima <37226050+thiagolima037@users.noreply.github.com>
2018-03-19 08:46:05 -0300

    Ex2 e Ex3

--- ex1.py
+++ ex1.py
@@ -1,6 +1,6 @@
 print("Hello World!")
 print("Hello again")
-print(" I like typing this.")
+print("I like typing this.")
 print("This is fun.")
 print('Yay! Printing.')
 print("I'd much rather you 'not'.")
